443254e608a23ce96513d1f317df49f7355708cd
Alexandru Filimon <a.filimon@alumnos.upm.es>
2023-11-28 22:35:05 +0100

    links

---
+++ HandsOn/Group03/misc/SPARQL/triplesCleanup.py
@@ -0,0 +1,30 @@
+def has_even_angle_brackets(line):
+    count_open = line.count("<")
+    count_close = line.count(">")
+    return count_open == count_close
+
+def get_lines_with_odd_brackets(file_path, output_file_path):
+    suspicious_lines = []
+    closed_lines = []
+
+    with open(file_path, 'r',encoding='utf-8') as file:
+        for line_number, line in enumerate(file, start=1):
+            if not has_even_angle_brackets(line):
+                suspicious_lines.append((line_number, line.strip()))
+            else:
+                closed_lines.append(line)
+
+    # Save lines with even brackets to a new file
+    with open(output_file_path, 'w',encoding='utf-8') as output_file:
+        output_file.writelines(closed_lines)
+
+    return suspicious_lines
+
+input_file_path = 'papeleras.nt'  # Replace with the path to your input file
+output_file_path = 'clean_papeleras.nt'  # Replace with the desired output file path
+result = get_lines_with_odd_brackets(input_file_path, output_file_path)
+
+for line_number, line in result:
+    print(f"Line {line_number}: {line}")
+
+print(f"Lines with even brackets saved to {output_file_path}")
--- HandsOn/Group03/selfAssessmentHandsOn4.md
+++ HandsOn/Group03/selfAssessmentHandsOn4.md
@@ -12,8 +12,8 @@
 **Every URI in the RDF files:**
 
 - [X] Is "readable" and has some meaning (e.g., it is not an auto-increased integer) 
-- [ ] Is not encoded as a string
-- [ ] Does not contain a double slash (i.e., “//”)
+- [X] Is not encoded as a string
+- [X] Does not contain a double slash (i.e., “//”)
 
 **Every individual in the RDF files:**
 
@@ -22,10 +22,10 @@
 
 **Every value in the RDF files:**
 
-- [ ] Is trimmed
-- [ ] Is properly encoded (e.g., dates, booleans)
-- [ ] Includes its datatype
-- [ ] Uses the correct datatype (e.g., values of 0-1 may be booleans and not integers, not every string made of numbers is a number)
+- [X] Is trimmed
+- [X] Is properly encoded (e.g., dates, booleans)
+- [X] Includes its datatype
+- [X] Uses the correct datatype (e.g., values of 0-1 may be booleans and not integers, not every string made of numbers is a number)
 
 ## Comments on the self-assessment
 _(If required)_
